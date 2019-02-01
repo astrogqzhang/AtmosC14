e3442b1460b391898e78d64e7c66f011841b6407
gqzhang <dugu_zgq@outlook.com>
2019-02-01 16:10:53 +0800

    I want to fix a bug but i failed

--- src/Constant.cc
+++ src/Constant.cc
@@ -1,8 +1,6 @@
 #include "Constant.hh"
 #include <vector>
 
-std::vector<int> NumberOfC14::vecNumberOfC14;
-
 NumberOfC14::NumberOfC14() {}
 NumberOfC14::~NumberOfC14() {}
 
@@ -10,10 +8,10 @@ NumberOfC14::~NumberOfC14() {}
 
 void NumberOfC14::push_back(const int& number)
 {
-    this->vecNumberOfC14.push_back(number);
+    vecNumberOfC14.push_back(number);
 }
 
 std::vector<int>& NumberOfC14::GetNumberOfC14()
 {
-    return this->vecNumberOfC14;
+    return vecNumberOfC14;
 }
